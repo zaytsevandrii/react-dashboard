ed9ff194aeb67d8635115e322ea31826fd207ee8
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2022-12-25 14:41:26 +0000

    create navbar component

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import { FiSettings } from "react-icons/fi"
 import { TooltipComponent } from "@syncfusion/ej2-react-popups"
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
+import { useStateContext } from "./contexts/ContextProvider";
 
 function App() {
-    const activeMenu = true
+    const {activeMenu} = useStateContext()
     return (
         <div>
            <BrowserRouter>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
-import App from './App';
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+import { ContextProvider } from "./contexts/ContextProvider"
+import "./index.css"
 
-ReactDOM.render(<App />, document.getElementById('root'))
-
+ReactDOM.render(
+    <ContextProvider>
+        <App />
+    </ContextProvider>,
+    document.getElementById("root")
+)
